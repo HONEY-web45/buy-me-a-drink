d9f201e3ab2a382ad46dfaf222fb3a3db79074c0
Honey <nitik2643@gmail.com>
2025-06-03 14:21:55 +0530

    update

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -35,6 +35,7 @@ export const authoptions =  NextAuth({
     //     from: 'NextAuth.js <no-reply@example.com>'
     //   }),
     ],
+    secret: process.env.NEXTAUTH_SECRET,
     callbacks: {
       async signIn({ user, account, profile, email, credentials }) {
          if(account.provider == "github" || account.provider == "google" || account.provider == "facebook"){ { 
