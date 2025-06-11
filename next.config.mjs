47d0b3ed3a284642bf5a6486955dca32c3070418
Honey <nitik2643@gmail.com>
2025-06-11 14:49:52 +0530

    P

--- next.config.mjs
+++ next.config.mjs
@@ -2,18 +2,9 @@
 const nextConfig = { 
     reactStrictMode: false,
      output: "standalone",
-      optimization: {
-    splitChunks: {
-      chunks: "all",
-    },
-     webpack(config) {
-    config.optimization.splitChunks = {
-      chunks: "all",
-      maxSize: 25000000, // Limit chunks to 25MB
-    };
-    return config;
-  }
-  },
+     
+    
+  
     
     };
 
