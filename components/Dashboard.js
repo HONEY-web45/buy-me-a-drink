c05b76489b581749b8de04bedb6e2040eca87dba
Honey <nitik2643@gmail.com>
2025-08-06 17:37:29 +0530

    p

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -63,7 +63,57 @@ const getdata=async()=>{
 
 
 
+ useEffect(() => {
+     let tl=gsap.timeline()
 
+     tl.from(".one",{
+      scale:0,
+      opacity:0,
+      duration:.5,
+      // rotate:180,
+      y:-100
+     })
+     tl.from(".a",{
+      // x:-200,
+      opacity:0,
+      duration:.1
+     })
+     tl.from(".b",{
+      // x:200,
+      opacity:0,
+      duration:.1
+     })
+     tl.from(".c",{
+      // x:-200,
+      opacity:0,
+      duration:.1
+     })
+     tl.from(".d",{
+      // x:200,
+      opacity:0,
+      duration:.1
+     })
+     tl.from(".e",{
+      // x:-200,
+      opacity:0,
+      duration:.1
+     })
+     tl.from(".f",{
+      // x:200,
+      opacity:0,
+      duration:.1
+     })
+     tl.from(".g",{
+      // x:-200,
+      opacity:0,
+      duration:.1
+     })
+     tl.from(".h",{
+      y:100,
+      opacity:0,
+      duration:.2
+     })
+     }, [])
 
 
 
@@ -216,31 +266,31 @@ theme="light"
 transition={Bounce}
 
 />
-    <h1 className='text-3xl text-center font-bold px-5 pb-10'>Welcome to your Dashboard</h1>
+    <h1 className='text-3xl text-center font-bold px-5 pb-10 one'>Welcome to your Dashboard</h1>
 
     <form className=' w-[80%] xl:w-[50%] flex flex-col gap-2' action={submit}>
-      <label htmlFor="name" className=' font-medium mx-1 text-lg '>Name</label>
-      <input type="text" id='name' onChange={(e)=>change(e)} name="name" value={form.name}  className='w-full bg-slate-700/50 rounded-xl p-2'  />
+      <label htmlFor="name" className=' font-medium mx-1 text-lg a'>Name</label>
+      <input type="text" id='name' onChange={(e)=>change(e)} name="name" value={form.name}  className='w-full bg-slate-700/50 rounded-xl p-2 a'  />
 
-      <label htmlFor="email" className=' font-medium mx-1 text-lg '>Email</label>
-      <input type="text"  id='email' readOnly={true}  name='email' value={form.email}  className='w-full placeholder:text-white bg-slate-700/50 rounded-xl p-2'  />
+      <label htmlFor="email" className=' font-medium mx-1 text-lg b'>Email</label>
+      <input type="text"  id='email' readOnly={true}  name='email' value={form.email}  className='w-full placeholder:text-white bg-slate-700/50 rounded-xl p-2 b'  />
 
-      <label htmlFor="username" className=' font-medium mx-1 text-lg '>Username</label>
-      <input type="text" id='username'  onChange={(e)=>change(e)} name='username' value={form.username?form.username:""} className='w-full bg-slate-700/50 rounded-xl p-2'  />
+      <label htmlFor="username" className=' font-medium mx-1 text-lg c'>Username</label>
+      <input type="text" id='username'  onChange={(e)=>change(e)} name='username' value={form.username?form.username:""} className='w-full bg-slate-700/50 rounded-xl p-2 c'  />
 
-      <label htmlFor="profile" className=' font-medium mx-1 text-lg '>Profile Picture</label>
-      <input type="text" onChange={(e)=>change(e)} name='profilepic' value={form.profilepic?form.profilepic:""} id='profile' className='w-full bg-slate-700/50 rounded-xl p-2'  />
+      <label htmlFor="profile" className=' font-medium mx-1 text-lg d'>Profile Picture</label>
+      <input type="text" onChange={(e)=>change(e)} name='profilepic' value={form.profilepic?form.profilepic:""} id='profile' className='w-full bg-slate-700/50 rounded-xl p-2 d'  />
 
-      <label htmlFor="cov"  className=' font-medium mx-1 text-lg'>Cover Picture</label>
-      <input type="text" onChange={(e)=>change(e)} name='coverpic' value={form.coverpic?form.coverpic:""} id='cov' className='w-full bg-slate-700/50 rounded-xl p-2'  />
+      <label htmlFor="cov"  className=' font-medium mx-1 text-lg e'>Cover Picture</label>
+      <input type="text" onChange={(e)=>change(e)} name='coverpic' value={form.coverpic?form.coverpic:""} id='cov' className='w-full bg-slate-700/50 rounded-xl p-2 e'  />
 
-      <label htmlFor="id" className=' font-medium mx-1 text-lg '>Razorpay Id</label>
-      <input type="text" id='id' onChange={(e)=>change(e)} name='id'  value={form.id?form.id:""} className='w-full bg-slate-700/50 rounded-xl p-2'  />
+      <label htmlFor="id" className=' font-medium mx-1 text-lg f'>Razorpay Id</label>
+      <input type="text" id='id' onChange={(e)=>change(e)} name='id'  value={form.id?form.id:""} className='w-full bg-slate-700/50 rounded-xl p-2 f'  />
 
-      <label htmlFor="secret" className=' font-medium mx-1 text-lg '>Razorpay Secret</label>
-      <input type="text" onChange={(e)=>change(e)} id='secret' name='secret' value={form.secret?form.secret:""} className='w-full bg-slate-700/50 rounded-xl p-2'  />
+      <label htmlFor="secret" className=' font-medium mx-1 text-lg g'>Razorpay Secret</label>
+      <input type="text" onChange={(e)=>change(e)} id='secret' name='secret' value={form.secret?form.secret:""} className='w-full bg-slate-700/50 rounded-xl p-2 g'  />
       
-<button type='submit'  className="text-white bg-gradient-to-br from-black  to-slate-700 hover:bg-gradient-to-bl hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-bold rounded-lg  text-lg px-5 py-2 justify-center my-2   inline-flex items-center dark:bg-blue-600  dark:hover:bg-blue-700 dark:focus:ring-slate-800 w-full" >Save</button>
+<button type='submit'  className="text-white bg-gradient-to-br from-black  to-slate-700 hover:bg-gradient-to-bl hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-bold rounded-lg  text-lg px-5 py-2 justify-center my-2   inline-flex items-center dark:bg-blue-600  dark:hover:bg-blue-700 dark:focus:ring-slate-800 w-full h" >Save</button>
 
 
 
